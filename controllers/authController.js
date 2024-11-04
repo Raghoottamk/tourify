5eaf25b404561b1a7f4e3c601377c4c1c90cf095
Raghoottamk <raghukulkarni2004@gmail.com>
2024-11-04 11:10:35 +0530

    completed geodspatial queries and reviews

--- controllers/authController.js
+++ controllers/authController.js
@@ -122,7 +122,6 @@ exports.restrictTo = (...roles) => {
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTed email
-
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(new AppError('There is no user with email address.', 404));
