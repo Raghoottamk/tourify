8fcaf9dc0e220cb5788a28c82728af7f4c3d97ac
Raghoottamk <raghukulkarni2004@gmail.com>
2024-11-15 02:02:09 +0530

    safe state

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,19 +1,31 @@
-/* eslint-disable */
-
 import axios from "axios";
 import { showAlert } from "./alerts";
+import { loadStripe } from "@stripe/stripe-js";
+import { STRIPE_PUBLISHABLE_KEY } from "../../config.js";
 
-const stripe = Stripe(
-  "pk_test_51PcWeKESEacLDRhzmR5DyV6Vq1cZnwzbUUHPHjVObOBSxJUpHkmsGDwckbFdCpkq0oVJ6xZZkCHANJ2sNv2Kzrx500sW8G1gxL"
-);
+// Initialize Stripe
+const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY); // Replace with your actual public key
 
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
-    // console.log(session);
-
-    // 2) Create checkout form + charge credit card
+    console.log(session);
+    if (!session.data.session.id) {
+      console.error("Session ID is undefined.");
+      showAlert("error", "Failed to retrieve session ID.");
+      return;
+    }
+    // 2) Ensure the Stripe object is loaded before using it
+    console.log("stripePromise", stripePromise);
+    const stripe = await stripePromise; // Await stripePromise to get the Stripe instance
+    if (!stripe) {
+      console.error("Stripe failed to initialize.");
+      showAlert("error", "Stripe failed to initialize.");
+      return;
+    }
+    console.log("stripe", stripe);
+    // 3) Redirect to Stripe Checkout
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
